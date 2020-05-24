0f336143485b3dc12e3d7226ef3958f78be9c8ca
ashokkumar1408 <kirthik14aug@gmail.com>
2020-05-24 14:51:26 +0100

    ashok test commit

---
+++ sustain-app/src/components/score.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ReactSpeedometer from "react-d3-speedometer";
+
+class Score extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 30,
+    };
+  }
+  render() {
+    return (
+        <div className="center">
+        <ReactSpeedometer
+        value={this.state.score}
+        maxSegmentLabels={10}
+        segments={200}
+        minValue={0}
+        maxValue={100}
+      />
+      </div>
+    );
+  }
+}
+
+export default Score;
