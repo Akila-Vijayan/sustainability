e490b90f259119e07a3172cb7d7eae6972093d00
Akila-Vijayan <60947626+Akila-Vijayan@users.noreply.github.com>
2020-06-05 18:59:27 +0100

    Update App.js

--- sustain-app/src/App.js
+++ sustain-app/src/App.js
@@ -1,24 +1,25 @@
 import React,{Component} from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Header from "./components/header";
 import Home from "./components/home";
 import SignUp from "./components/signup";
 import CheckList from "./components/checklist";
-import Score from './components/score';
+import Score from "./components/score";
 
 class App extends Component {
-constructor(props){
-  super(props);
+  constructor(props){
+    super(props);
 
-  this.updateScore=this.updateScore.bind(this);
+    this.updateScore=this.updateScore.bind(this);
 
-  this.state={score:0};
-}
-updateScore = (score) => {
-  this.setState({score});
-}
+    this.state={score:0};
+  }
+
+  updateScore = (score) => {
+    this.setState({score});
+  }
 
   render(){
     const {score} =this.state;
@@ -28,7 +29,7 @@ updateScore = (score) => {
           <Header />
           <Route path="/" exact component={Home} />
           <Route path="/signup" component={SignUp} />
-          <Route path="/creditScore" render={(props)=> <CheckList {...props} updateScore={this.updateScore}/>} />
+          <Route path="/checkList" render={(props)=> <CheckList {...props} updateScore={this.updateScore}/>} />
           <Route path="/score" render={(props)=> <Score score={score}/>}/>
         </div>
       </Router>
