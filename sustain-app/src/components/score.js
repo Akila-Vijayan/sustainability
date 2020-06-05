a5e6b32f3b4595bb3a77f9fc01f2f1cf59720734
Akila-Vijayan <60947626+Akila-Vijayan@users.noreply.github.com>
2020-06-05 18:58:21 +0100

    Added sample product table

--- sustain-app/src/components/score.js
+++ sustain-app/src/components/score.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactSpeedometer from "react-d3-speedometer";
+import Table from 'react-bootstrap/Table'
 
 class Score extends Component {
   constructor(props) {
@@ -9,23 +10,40 @@ class Score extends Component {
     };
   }
   render() {
-    const style = {
-      color: "Black",
-      padding: "10px",
-      fontFamily: "Calibri (Body)"
-    };
-    return (    
-        <div align="center"><br/><br/><br/>
-        <h4 style={style} >
-        Your Sustainabilty Score is {this.state.score}<br/><p></p>
-         </h4>
-        <ReactSpeedometer
-        value={this.state.score}
-        maxSegmentLabels={10}
-        segments={200}
-        minValue={0}
-        maxValue={100}
-      />
+    return (
+        <div align="center">
+          <h3> Your Sustainability Score is {this.state.score} </h3>
+          <hr/>
+          <div>
+            <ReactSpeedometer
+            value={this.state.score}
+            maxSegmentLabels={10}
+            segments={100}
+            minValue={0}
+            maxValue={100}
+            width={500}/>
+          </div>
+          <div align = "left">
+            To increase your sustainability score, please opt for the below Lloyds Products,<br/><br/>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th> Products </th>
+                  <th> Offers </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Green Credit Card</td>
+                  <td>Exclusively for buying solar panels or heaters</td>
+                </tr>
+                <tr>
+                  <td>Mortgages – Green Offers</td>
+                  <td>When buying house with eco-friendly amenities, will provide offers on interest rates</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
       </div>
     );
   }
